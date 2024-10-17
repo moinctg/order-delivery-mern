a36fb54e714a16da633060f14b83d2b0c7490762
Md Moinuddin Kamal <cpimoinuddin@gmail.com>
2024-10-17 14:22:23 +0600

    Update url to server link

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = 'http://localhost:4000';
+  const url = 'https://order-delivery-mern-backend-1.onrender.com';
 
   return (
     <div>
